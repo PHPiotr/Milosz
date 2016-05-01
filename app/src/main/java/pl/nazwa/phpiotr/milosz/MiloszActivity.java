074d7356378a80be44712fb9e3a6aca5cf25df49
Piotr Kowalski <piet.kowalski@gmail.com>
2016-05-01 02:56:18 +0200

    SeekBar challenge. Trying to control sound being currently played.

--- app/src/main/java/pl/nazwa/phpiotr/milosz/MiloszActivity.java
+++ app/src/main/java/pl/nazwa/phpiotr/milosz/MiloszActivity.java
@@ -10,10 +10,21 @@ import android.widget.SeekBar;
 
 public class MiloszActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
+    private final int COUNT = 10;
+    private final String TAG = this.getClass().getSimpleName();
     private Thread threadInClick = null;
+    private Thread threadInSeek = null;
     private MediaPlayer mediaPlayerInClick = null;
-    private SeekBar seekBar = null;
-    private Handler handler = null;
+    private MediaPlayer mediaPlayerInSeek = null;
+    private SeekBar seekbars[] = new SeekBar[COUNT];
+    private ImageButton buttons[] = new ImageButton[COUNT];
+    private MediaPlayer players[] = new MediaPlayer[COUNT];
+    private Handler clickHandler = null;
+    private Handler seekHandler = null;
+    private int currentButtonIndex = -1;
+    private int currentSeekBarIndex = -1;
+    private boolean isSeeked = false;
+    private int playerProgress = -1;
 
     private int[] mp3toPng = {
             R.raw.aga_milosz,
@@ -59,11 +70,15 @@ public class MiloszActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_milosz);
 
+        int i = 0;
         for (int id : ids) {
             ImageButton imageButton = (ImageButton) findViewById(id);
             if (imageButton != null) {
+                seekbars[i] = (SeekBar) findViewById(seekIds[i]);
+                seekbars[i].setOnSeekBarChangeListener(this);
                 imageButton.setOnClickListener(this);
             }
+            i++;
         }
 
     }
@@ -76,57 +91,106 @@ public class MiloszActivity extends AppCompatActivity implements View.OnClickLis
             threadInClick = null;
         }
 
-        if (mediaPlayerInClick != null && mediaPlayerInClick.isPlaying()) {
-            mediaPlayerInClick.stop();
-            mediaPlayerInClick.reset();
-            mediaPlayerInClick.release();
-            mediaPlayerInClick = null;
-        }
-
         for (int i = 0; i < ids.length; i++) {
-
+            if (players[i] != null && players[i].isPlaying()) {
+                players[i].stop();
+                players[i].reset();
+                players[i].release();
+                players[i] = null;
+            }
             if (ids[i] == v.getId()) {
-
-                final int soundId = mp3toPng[i];
-                final int seekId = seekIds[i];
-                mediaPlayerInClick = MediaPlayer.create(MiloszActivity.this, soundId);
-                seekBar = (SeekBar) findViewById(seekId);
-                if (seekBar != null) {
-                    seekBar.setMax(mediaPlayerInClick.getDuration());
-                }
-                mediaPlayerInClick.start();
-
-                handler = new Handler();
-
-                threadInClick = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (mediaPlayerInClick.isPlaying()) {
-                            seekBar.setProgress(mediaPlayerInClick.getCurrentPosition());
-                            handler.postDelayed(this, 50);
-                        }
-                    }
-                });
-
-                threadInClick.start();
-
-                break;
+                currentButtonIndex = i;
             }
         }
+
+        if (currentButtonIndex == -1) {
+            return;
+        }
+
+        players[currentButtonIndex] = MediaPlayer.create(MiloszActivity.this, mp3toPng[currentButtonIndex]);
+
+        if (seekbars[currentButtonIndex] != null) {
+            seekbars[currentButtonIndex].setMax(players[currentButtonIndex].getDuration());
+        }
+
+        players[currentButtonIndex].start();
+
+        clickHandler = new Handler();
+
+        threadInClick = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (players[currentButtonIndex] != null && players[currentButtonIndex].isPlaying()) {
+                    seekbars[currentButtonIndex].setProgress(players[currentButtonIndex].getCurrentPosition());
+                    clickHandler.postDelayed(this, 50);
+                }
+            }
+        });
+
+        threadInClick.start();
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+        if (isSeeked && players[currentSeekBarIndex] != null) {
+            playerProgress = progress;
+        }
     }
 
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
+
+        for (int i = 0; i < seekIds.length; i++) {
+            if (seekIds[i] != seekBar.getId()) {
+                continue;
+            }
+            currentSeekBarIndex = i;
+        }
+
+        if (currentSeekBarIndex == -1) {
+            return;
+        }
+
+        // seek bar of sound being currently played
+        if (players[currentSeekBarIndex] != null && players[currentSeekBarIndex].isPlaying()) {
+            isSeeked = true;
+            players[currentSeekBarIndex].pause();
+            return;
+        }
+
+        isSeeked = false;
 
     }
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
 
+        if (isSeeked && players[currentSeekBarIndex] != null && playerProgress != -1 && !players[currentSeekBarIndex].isPlaying()) {
+            mediaPlayerInSeek = null;
+            mediaPlayerInSeek = players[currentSeekBarIndex];
+            mediaPlayerInSeek.seekTo(playerProgress);
+            mediaPlayerInSeek.start();
+
+            if (seekHandler == null) {
+                seekHandler = new Handler();
+            }
+
+            if (threadInSeek != null) {
+                threadInSeek.interrupt();
+                threadInSeek = null;
+            }
+
+            threadInSeek = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mediaPlayerInSeek != null && mediaPlayerInSeek.isPlaying()) {
+                        seekbars[currentSeekBarIndex].setProgress(mediaPlayerInSeek.getCurrentPosition());
+                        seekHandler.postDelayed(this, 50);
+                    }
+                }
+            });
+
+            threadInSeek.start();
+        }
     }
 }
